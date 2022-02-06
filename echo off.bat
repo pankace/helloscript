98d8f637dabd18ffd016f37723b779a594284b08
pankace <44595774+pankace@users.noreply.github.com>
2022-02-06 14:53:00 +0700

    adde file

---
+++ echo off.bat
@@ -0,0 +1,25 @@
+echo on 
+echo hello world 
+pause 
+color a
+pause 
+dir
+ipconfig /all
+ping hacking.com
+tracert hacking.com
+Invoke-WebRequest -Uri www.google.com
+pause 
+::  d8b        d8,      d8,                                                d8,                          d8,     d8b   d8, d8b                                       d8b                           d8b                     
+:: ?88       `8P      `8P                                                `8P                          `8P      88P  `8P  ?88                                       88P                           88P            d8P      
+::  88b                                                                                                       d88         88b                                     d88                           d88          d888888P    
+::  888888b   88b      88b     d888b8b    88bd8b,d88b       88bd8b,d88b   88b  88bd88b  d888b8b        88b    888    88b  888  d88' d8888b     d8888b d8888b  d888888   d8888b     d888b8b      888   d8888b   ?88'      
+::  88P `?8b  88P      88P    d8P' ?88    88P'`?8P'?8b      88P'`?8P'?8b  88P  88P' ?8bd8P' ?88        88P    ?88    88P  888bd8P' d8b_,dP    d8P' `Pd8P' ?88d8P' ?88  d8b_,dP    d8P' ?88      ?88  d8P' ?88  88P       
+:: d88   88P d88      d88     88b  ,88b  d88  d88  88P     d88  d88  88P d88  d88   88P88b  ,88b      d88      88b  d88  d88888b   88b        88b    88b  d8888b  ,88b 88b        88b  ,88b      88b 88b  d88  88b       
+::d88'   88bd88'     d88'     `?88P'`88bd88' d88'  88b    d88' d88'  88bd88' d88'   88b`?88P'`88b    d88'       88bd88' d88' `?88b,`?888P'    `?888P'`?8888P'`?88P'`88b`?888P'    `?88P'`88b      88b`?8888P'  `?8b      
+::                                                                                            )88                                                                                                                        
+::                                                                                           ,88P                                                                                                                        
+::                                                                                       `?8888P                                                                                                                       
+
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+PAUSE
+
